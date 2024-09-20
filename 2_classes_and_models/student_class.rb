69955c299139ae7c5cadbd4461fbff6075f15362
MoiBeTeroK <ludarikkot@gmail.com>
2024-09-21 01:16:52 +0300

    Add attributes for short writing of getters and setters(task 1.3-1.4)

--- 2_classes_and_models/student_class.rb
+++ 2_classes_and_models/student_class.rb
@@ -1,4 +1,7 @@
 class Student
+
+  attr_accessor :surname, :name, :patronymic, :id, :phone, :telegram, :email, :git
+
   def initialize(surname, name, patronymic, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
     @id = id
     @surname = surname
@@ -7,72 +10,6 @@ class Student
     @phone = phone
     @telegram = telegram
     @email = email
-    @git = git
-  end
-
-   # Геттеры
-   def id
-    @id
-  end
-
-  def surname
-    @surname
-  end
-
-  def name
-    @name
-  end
-
-  def patronymic
-    @patronymic
-  end
-
-  def phone
-    @phone
-  end
-
-  def telegram
-    @telegram
-  end
-
-  def email
-    @email
-  end
-
-  def git
-    @git
-  end
-
-  # Сеттеры
-  def set_id=(id)
-    @id = id
-  end
-
-  def set_surname=(surname)
-    @surname = surname
-  end
-
-  def set_name=(name)
-    @name = name
-  end
-
-  def set_patronymic=(patronymic)
-    @patronymic = patronymic
-  end
-
-  def set_phone=(phone)
-    @phone = phone
-  end
-
-  def set_telegram=(telegram)
-    @telegram = telegram
-  end
-
-  def set_email=(email)
-    @email = email
-  end
-
-  def set_git=(git)
     @git = git
   end
 
