128a94d71f245a2dd6df72b8a5eb824a9f970908
MoiBeTeroK <ludarikkot@gmail.com>
2024-09-21 01:42:11 +0300

    Add the ability to create objects with any combination of fields(task 1.5)

--- 2_classes_and_models/main.rb
+++ 2_classes_and_models/main.rb
@@ -1,8 +1,8 @@
 require './student_class.rb'
 
-student1 = Student.new("Ivanov", "Ivan", "Ivanovich", 1, "80123456789", "ivan1337", "ivanov@gmail.com", "github.com/ivanov")
-student2 = Student.new("Sidorov", "Dmitry", "Egorovich", 2, "81234567890", "prosto_tosto", "dmsid01@gmail.com", "github.com/robbot22")
-student3 = Student.new("Petrov", "Andrey", "Dmitrievich", 3, "82345678901", "andy_panda", "andr123@gmail.com", "github.com/pojiloi_shmel")
+student1 = Student.new(surname:"Ivanov", name:"Ivan", patronymic:"Ivanovich", id:1, phone:"80123456789", telegram:"ivan1337", email:"ivanov@gmail.com", git:"github.com/ivanov")
+student2 = Student.new(surname:"Sidorov", name:"Dmitry", patronymic:"Egorovich", id:2, phone:"81234567890", telegram:"prosto_tosto", email:"dmsid01@gmail.com", git:"github.com/robbot22")
+student3 = Student.new(surname:"Petrov", name:"Andrey", patronymic:"Dmitrievich", id:3, phone:"82345678901", telegram:"andy_panda", email:"andr123@gmail.com", git:"github.com/pojiloi_shmel")
 
 student1.information()
 student2.information()
--- 2_classes_and_models/student_class.rb
+++ 2_classes_and_models/student_class.rb
@@ -2,15 +2,17 @@ class Student
 
   attr_accessor :surname, :name, :patronymic, :id, :phone, :telegram, :email, :git
 
-  def initialize(surname, name, patronymic, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
-    @id = id
-    @surname = surname
-    @name = name
-    @patronymic = patronymic
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
+  def initialize(parameters)
+    @surname = parameters[:surname]
+    @name = parameters[:name]
+    @patronymic = parameters[:patronymic]
+    @id = parameters[:id]
+    @phone = parameters[:phone]
+    @telegram = parameters[:telegram]
+    @email = parameters[:email]
+    @git = parameters[:git]
+
+    raise "Surname, name and patronymic cannot be empty" if @surname.nil? || @name.nil? || @patronymic.nil?
   end
 
   def information()
