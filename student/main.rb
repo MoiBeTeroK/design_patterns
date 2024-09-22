83753994e214a857c68d29e3268d684f6d32618e
MoiBeTeroK <ludarikkot@gmail.com>
2024-09-22 22:07:22 +0300

    Add validations for other fields(task 1.8)

--- student/main.rb
+++ student/main.rb
@@ -1,8 +1,8 @@
 require './student_class.rb'
 
-student1 = Student.new(surname:"Ivanov", name:"Ivan", patronymic:"Ivanovich", id:1, phone:"80123456789", telegram:"ivan1337", email:"ivanov@gmail.com", git:"github.com/ivanov")
-student2 = Student.new(surname:"Sidorov", name:"Dmitry", patronymic:"Egorovich", id:2, phone:"81234567890", telegram:"prosto_tosto", email:"dmsid01@gmail.com", git:"github.com/robbot22")
-student3 = Student.new(surname:"Petrov", name:"Andrey", patronymic:"Dmitrievich", id:3, phone:"82345678901", telegram:"andy_panda", email:"andr123@gmail.com", git:"github.com/pojiloi_shmel")
+student1 = Student.new(surname:"Ivanov", name:"Ivan", patronymic:"Ivanovich", id:1, phone:"80123456789", telegram:"@ivan1337", email:"ivanov@gmail.com", git:"github.com/ivanov")
+student2 = Student.new(surname:"Sidorov", name:"Dmitry", patronymic:"Egorovich", id:2, phone:"81234567890", telegram:"@prosto_tosto", email:"dmsid01@gmail.com", git:"github.com/robbot22")
+student3 = Student.new(surname:"Petrov", name:"Andrey", patronymic:"Dmitrievich", id:3, phone:"82345678901", telegram:"@andy_panda", email:"andr123@gmail.com", git:"github.com/pojiloi_shmel")
 
 student1.information()
 student2.information()
--- student/student_class.rb
+++ student/student_class.rb
@@ -1,7 +1,7 @@
 class Student
 
   attr_accessor :surname, :name, :patronymic, :id, :telegram, :email, :git
-  attr_reader :phone
+  attr_reader :phone, :telegram, :email, :git
 
   def initialize(parameters)
     @surname = parameters[:surname]
@@ -9,9 +9,9 @@ class Student
     @patronymic = parameters[:patronymic]
     @id = parameters[:id]
     self.phone = parameters[:phone]
-    @telegram = parameters[:telegram]
-    @email = parameters[:email]
-    @git = parameters[:git]
+    self.telegram = parameters[:telegram]
+    self.email = parameters[:email]
+    self.git = parameters[:git]
 
     raise "Surname, name and patronymic cannot be empty" if @surname.nil? || @name.nil? || @patronymic.nil?
   end
@@ -20,9 +20,36 @@ class Student
     phone.match(/\A(\+?7|8)\d{10}\z/)
   end
 
+  def self.email_valid?(email)
+    email.match?(/\A[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+\z/)
+  end
+
+  def self.telegram_valid?(telegram)
+    telegram.match?(/\A@[a-zA-Z0-9_]{5,}\z/)
+  end
+
+  def self.git_valid?(git)
+    git.match?(/\A(https:\/\/)?github.com\/[a-zA-Z0-9_-]+\z/)
+  end
+
   def phone=(phone)
     raise "Incorrect phone number" if !Student.phone_number?(phone)
     @phone = phone
+  end
+
+  def telegram=(telegram)
+    raise "Incorrect telegram" if !Student.telegram_valid?(telegram)
+    @telegram=telegram
+  end
+
+  def email=(email)
+    raise "Incorrect email"if !Student.email_valid?(email)
+    @email=email
+  end
+
+  def git=(git)
+    raise "Incorrect git"if !Student.git_valid?(git)
+    @git=git
   end
 
   def information()
