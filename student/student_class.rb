a5678beb0842b95a7ef5e68202d39b49a0ab82d9
MoiBeTeroK <ludarikkot@gmail.com>
2024-09-22 20:39:32 +0300

    Add phone number verification(task 1.7)

--- student/student_class.rb
+++ student/student_class.rb
@@ -1,18 +1,28 @@
 class Student
 
-  attr_accessor :surname, :name, :patronymic, :id, :phone, :telegram, :email, :git
+  attr_accessor :surname, :name, :patronymic, :id, :telegram, :email, :git
+  attr_reader :phone
 
   def initialize(parameters)
     @surname = parameters[:surname]
     @name = parameters[:name]
     @patronymic = parameters[:patronymic]
     @id = parameters[:id]
-    @phone = parameters[:phone]
+    self.phone = parameters[:phone]
     @telegram = parameters[:telegram]
     @email = parameters[:email]
     @git = parameters[:git]
 
     raise "Surname, name and patronymic cannot be empty" if @surname.nil? || @name.nil? || @patronymic.nil?
+  end
+
+  def self.phone_number?(phone)
+    phone.match(/\A(\+?7|8)\d{10}\z/)
+  end
+
+  def phone=(phone)
+    raise "Incorrect phone number" if !Student.phone_number?(phone)
+    @phone = phone
   end
 
   def information()
