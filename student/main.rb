9af1a2945af01e0a30bf3e4ef54eb8f20209efa3
MoiBeTeroK <ludarikkot@gmail.com>
2024-10-12 01:58:45 +0300

    Fix some bugs

--- student/main.rb
+++ student/main.rb
@@ -4,13 +4,11 @@ student1 = Student.new(surname:"Ivanov", name:"Ivan", patronymic:"Ivanovich", id
 student2 = Student.new(surname:"Sidorov", name:"Dmitry", patronymic:"Egorovich", id:2, phone:"82398348901", git:"github.com/robbot22")
 student3 = Student.new(surname:"Petrov", name:"Andrey", patronymic:"Dmitrievich", id:3, phone:"82345678901", telegram:"@andy_panda", email:"andr123@gmail.com")
 
-puts student2.to_s
 
-
-student_short1 = StudentShort.new(id: 1, surname_initials: "Ivanov I.I.", git: "https://github.com/ivanov_short", contact: "+79991234567")
-
-info_string = "surname_initials:Ivanov I.I., git:github.com/ivanov_short, contact:89991234567"
-student_short2 = StudentShort.from_string(2, info_string)
+student_short1 = StudentShort.new(student2)
 
 info_student2=student2.get_info
-student_short3 = StudentShort.from_string(3, info_student2)
+student_short_get_info=StudentShort.from_string(3, info_student2)
+
+data = "surname_initials:Ivanov I. I., git:github.com/ivanov123, contact:89876543210"
+student_short2 = StudentShort.from_string(1, data)
--- student/student_class.rb
+++ student/student_class.rb
@@ -1,9 +1,8 @@
 class Person
 
-  def initialize(id:, git:nil)
+  def initialize(id:nil, git:nil)
     @id = id
     @git = git
-    raise ArgumentError, "Invalid git" if !self.class.git_valid?(@git)
   end
 
   def self.phone_number?(phone)
@@ -23,25 +22,32 @@ class Person
   end
 
   def self.fio_valid?(fio)
-    !fio.nil? && fio.match?(/^[A-Z][a-z]+\z/)
+    !fio.nil? && fio.match?(/^[A-Z][a-z]*\z/)
   end
 
+  def validate?
+    raise ArgumentError, "No git and contacts"
+  end
+  
+  def contact
+    raise ArgumentError, "No contacts"
+  end
 end
 
 class Student < Person
   attr_accessor :surname, :name, :patronymic, :id, :git
   attr_reader :phone, :telegram, :email
 
-  def initialize(id:, surname:, name:, patronymic:, phone: nil, telegram: nil, email: nil, git: nil)
+  def initialize(id:nil, surname:, name:, patronymic:, phone: nil, telegram: nil, email: nil, git: nil)
+    raise ArgumentError, "Surname cannot be empty" if !Student.fio_valid?(surname)
+    raise ArgumentError, "Name cannot be empty" if !Student.fio_valid?(name)
+    raise ArgumentError, "Patronymic cannot be empty" if !Student.fio_valid?(patronymic)
+    
     super(id:id, git:git)
     self.surname = surname
     self.name = name
     self.patronymic = patronymic
     set_contacts(phone:phone, telegram:telegram, email:email)
-    
-    raise ArgumentError, "Surname cannot be empty" if !self.class.fio_valid?(@surname)
-    raise ArgumentError, "Name cannot be empty" if !self.class.fio_valid?(@name)
-    raise ArgumentError, "Patronymic cannot be empty" if !self.class.fio_valid?(@patronymic)
   end
 
   def set_contacts(phone:nil, telegram:nil, email:nil)
@@ -55,14 +61,14 @@ class Student < Person
     "#{'-' * 40}"
   end
 
-  def get_contact
+  def contact
     return @phone if @phone
     return @telegram if @telegram
     return @email if @email
   end
 
   def get_info
-    "surname_initials:#{@surname} #{@name[0]}. #{@patronymic[0]}., git:#{@git || "No git"}, contact:#{get_contact}"
+    "surname_initials:#{@surname} #{@name[0]}. #{@patronymic[0]}., git:#{@git || "No git"}, contact:#{contact}"
   end
 
   def has_git?
@@ -82,40 +88,42 @@ class Student < Person
   private 
 
   def phone=(phone)
-    raise ArgumentError,"Incorrect phone number" if !self.class.phone_number?(phone)
+    raise ArgumentError,"Incorrect phone number" if !Student.phone_number?(phone)
     @phone = phone
   end
 
   def telegram=(telegram)
-    raise ArgumentError,"Incorrect telegram" if !self.class.telegram_valid?(telegram)
+    raise ArgumentError,"Incorrect telegram" if !Student.telegram_valid?(telegram)
     @telegram = telegram
   end
 
   def email=(email)
-    raise ArgumentError,"Incorrect email" if !self.class.email_valid?(email)
+    raise ArgumentError,"Incorrect email" if !Student.email_valid?(email)
     @email = email
   end
 
   def surname=(surname)
-    @surname = surname if self.class.fio_valid?(surname)
+    @surname = surname if Student.fio_valid?(surname)
   end
 
   def name=(name)
-    @name = name if self.class.fio_valid?(name)
+    @name = name if Student.fio_valid?(name)
   end
 
   def patronymic=(patronymic)
-    @patronymic = patronymic if self.class.fio_valid?(patronymic)
+    @patronymic = patronymic if Student.fio_valid?(patronymic)
   end
 end
 
 class StudentShort < Person
-  attr_reader :id, :surname_initials, :contact, :git
+  attr_reader :id, :git, :surname_initials, :contact
 
-  def initialize(id:, surname_initials:, git:nil, contact:nil)
-    super(id:id,git: git)
-    @surname_initials = surname_initials
-    @contact = contact
+  def initialize(student, contact: nil)
+    raise "The :surname_initials is missing" unless student.surname || student.name ||student.patronymic
+
+    super(id: student.id, git: student.git)
+    @surname_initials = "#{student.surname} #{student.name[0]}. #{student.patronymic[0]}."
+    @contact = student.phone || student.telegram || student.email
   end
 
   def self.from_string(id, info)
@@ -125,21 +133,25 @@ class StudentShort < Person
 
     info.split(",").each do |field|
       pair = field.split(":")
+      raise ArgumentError, "Invalid info" if pair.length != 2
 
-      if pair.length != 2
-        raise ArgumentError, "Invalid info"
-      end
-
-      if pair[0].strip == "surname_initials"
-        surname_initials= pair[1].strip
-      elsif pair[0].strip == "git"
-        git= pair[1].strip
-      elsif pair[0].strip == "contact"
-        contact= pair[1].strip
+      case pair[0].strip
+      when "surname_initials"
+        surname_initials = pair[1].strip
+      when "git"
+        git = pair[1].strip
+      when "contact"
+        contact = pair[1].strip
       else
         raise ArgumentError, "Invalid field"
       end
     end
-    StudentShort.new(id:id, surname_initials:surname_initials, git:git, contact:contact)
+    surname, name, patronymic = surname_initials.gsub('.', '').split(" ", 3)
+    student = Student.new(id:id, surname: surname, name: name, patronymic: patronymic, git: git)
+    student_sh = StudentShort.new(student, contact: contact)
+  end
+
+  def validate?
+    StudentShort.git_valid?(@git) && !@contact.nil?
   end
 end
