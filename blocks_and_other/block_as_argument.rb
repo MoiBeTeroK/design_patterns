7e2194fd1880a39852ad140a9e6b6d854aea8575
MoiBeTeroK <ludarikkot@gmail.com>
2024-10-18 01:52:48 +0300

    Add the choice of one of the methods

--- blocks_and_other/block_as_argument.rb
+++ blocks_and_other/block_as_argument.rb
@@ -3,7 +3,7 @@
 def read_from_file(file)
   begin
     File.open(file, 'r') do |f|
-      f.flat_map { |line| line.split.map{|x| x.to_f } }
+      f.flat_map { |line| line.split.map{|x| x.to_i } }
     end
   rescue
     puts "Такого адреса файла не существует"
@@ -16,6 +16,16 @@ def read_from_input
   input = gets.chomp 
   input.split.map(&:to_i)
 end
+
+# Меню для выбора задачи
+puts "Выберите задачу для решения:"
+puts "1 - Дан целочисленный массив и натуральный индекс (число, меньшее размера массива). Необходимо определить является ли элемент по указанному индексу глобальным минимумом."
+puts "2 - Дан целочисленный массив. Необходимо поменять местами минимальный и максимальный элементы массива??????????????????????"
+puts "3 - Дан целочисленный массив и интервал a..b. Необходимо проверить наличие максимального элемента массива в этом интервале"
+puts "4 - Дан целочисленный массив. Найти среднее арифметическое модулей его элементов"
+puts "5 - Для введенного списка построить новый с элементами, большими, чем среднее арифметическое списка, но меньшими, чем его максимальное значение"
+
+task_choice = gets.chomp.to_i
 
 puts "Способ ввода: 1 - с клавиатуры, 2 - из файла"
 choice = gets.chomp.to_i
@@ -33,36 +43,28 @@ end
 # 5 Дан целочисленный массив и натуральный индекс (число, меньшее размера массива). Необходимо определить является ли элемент по указанному индексу глобальным минимумом.
 
 def is_global_minimum?(array, index)
+  return false if array.empty?
   el = array[index]
   array.all? { |x| el <= x }
 end
 
-puts is_global_minimum?(array, 6)
-
 # 17 Дан целочисленный массив. Необходимо поменять местами минимальный и максимальный элементы массива.
 
 def swap_min_max(arr)
-  return arr if arr.empty?
-
+  return [] if arr.empty?
   min_index = arr.index(arr.min)
   max_index = arr.index(arr.max)
   arr[min_index], arr[max_index] = arr[max_index], arr[min_index]
   arr
 end
 
-puts "Исходный массив: #{array}"
-new_array = swap_min_max(array)
-puts "Преобразованный массив: #{new_array}"
-
 # 29 Дан целочисленный массив и интервал a..b. Необходимо проверить наличие максимального элемента массива в этом интервале.
 
 def max_in_range?(array, a, b)
-  max_el= array.max
-  return true if a <= max_el && max_el <= b
-  false
+  return false if array.empty?
+  max_el = array.max
+  in_range = a <= max_el && max_el <= b
 end
-
-puts max_in_range?(array, 1, 100)
 
 # 41 Дан целочисленный массив. Найти среднее арифметическое модулей его элементов.
 
@@ -73,8 +75,6 @@ def average_absolute(array)
   average = sum_abs.to_f / count
 end
 
-puts average_absolute(array)
-
 # 53 Для введенного списка построить новый с элементами, большими, чем среднее арифметическое списка, но меньшими, чем его максимальное значение.
 
 def modified_list(array)
@@ -84,4 +84,18 @@ def modified_list(array)
   new_array = array.select { |x| x > average && x < max_value }
 end
 
-print modified_list(array)
+case task_choice
+when 1
+  puts is_global_minimum?(array, 6)
+when 2 
+  puts "Исходный массив: #{array}"
+  new_array = swap_min_max(array)
+  puts "Преобразованный массив: #{new_array}"
+when 3 
+  puts max_in_range?(array, 1, 100)
+when 4 
+  puts average_absolute(array)
+when 5 
+  print modified_list(array)
+else puts "Неверный выбор задачи"
+end
